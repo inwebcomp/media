6c09da9f362593444e96ee4dc3fdb70e1898b0d9
Alexander Topalo <saneock19@gmail.com>
2019-12-29 23:58:42 +0200

    Payment

--- src/App/Images.php
+++ src/App/Images.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
-use InWeb\Media\Events\ImageAdded;
+use InWeb\Media\Events\PaymentCreated;
 use InWeb\Media\Events\ImageRemoved;
 
 class Images extends HasMany
@@ -122,7 +122,7 @@ class Images extends HasMany
             $image->createThumbnails();
         }
 
-        event(new ImageAdded($image));
+        event(new PaymentCreated($image));
 
         return $image;
     }
