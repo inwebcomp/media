e50240d23b803ffbb7fda34376b88c9a0b5b2d8d
Escral <inwebcomp@gmail.com>
2020-02-14 12:07:44 +0200

    Moved images to new namespace

--- src/database/migrations/2018_02_10_100000_create_images_table.php
+++ src/database/migrations/2018_02_10_100000_create_images_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-use InWeb\Media\Image;
+use InWeb\Media\Images\Image;
 
 class CreateImagesTable extends Migration
 {
