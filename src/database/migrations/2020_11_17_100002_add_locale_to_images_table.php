3e369f82657151dcd443683253ee776de8e1cdee
Escral <inwebcomp@gmail.com>
2020-11-18 03:26:39 +0200

    Database migrations

---
+++ src/database/migrations/2020_11_17_100002_add_locale_to_images_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use InWeb\Media\Images\Image;
+
+class AddLocaleToImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->string('language')->nullable()->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->dropColumn('language');
+        });
+    }
+}
