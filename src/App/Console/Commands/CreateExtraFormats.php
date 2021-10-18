d05996d3cea0b19f3b32339ca6b3e35293520555
Escral <inwebcomp@gmail.com>
2021-10-18 18:13:20 +0300

    Analyze extra formats

--- src/App/Console/Commands/CreateExtraFormats.php
+++ src/App/Console/Commands/CreateExtraFormats.php
@@ -15,7 +15,7 @@ class CreateExtraFormats extends Command
      *
      * @var string
      */
-    protected $signature = 'images:extra-formats';
+    protected $signature = 'images:extra-formats {--analyze}';
     /**
      * The console command description.
      *
@@ -40,22 +40,70 @@ class CreateExtraFormats extends Command
      */
     public function handle()
     {
-        $images = Image::all();
+        $images = Image::orderBy('model')->get();
 
         $this->getOutput()->progressStart(count($images));
 
-        $images->each(function(Image $image) {
+        $analyze = $this->option('analyze') ?: false;
+
+        $originalSize = 0;
+        $newSize = [];
+
+        $images->each(function (Image $image) use (&$newSize, &$originalSize, $analyze) {
             $object = $image->getObject();
+            $storage = $image->getStorage();
 
             foreach ($object->extraFormats() as $item) {
-                $image->createExtraFormatFile($item['format'], $item['quality']);
+                $format = $item['format'];
+                $quality = $item['quality'];
+
+                if ($analyze) {
+                    $originalSize += $storage->size($image->getPath());
+                }
+
+                $image->createExtraFormatFile($format, $quality);
+
+                if ($analyze) {
+                    if (! isset($newSize[$format]))
+                        $newSize[$format] = 0;
+
+                    $newSize[$format] += $storage->size($image->getPath('original', $format));
+                }
 
                 foreach ($object->getImageThumbnails() as $thumbnail => $info) {
-                    $image->createExtraFormatFile($item['format'], $item['quality'], $thumbnail);
+                    $image->createExtraFormatFile($format, $quality, $thumbnail);
                 }
             }
 
             $this->getOutput()->progressAdvance(1);
         });
+
+        if ($analyze) {
+            $this->getOutput()->newLine(2);
+
+            foreach ($newSize as $format => $size) {
+                $this->getOutput()->table(['Default', ucfirst($format)], [
+                    [$this->formatBytes($originalSize, 2), $this->formatBytes($size, 2)]
+                ]);
+
+                $diff = $originalSize - $size;
+                $this->getOutput()->success("Saved space (" . ucfirst($format) . "): " . $this->formatBytes($diff, 2) . ' (' . round($diff / ($originalSize / 100)) . '%)');
+            }
+        }
+    }
+
+    private function formatBytes($bytes, $precision = 2)
+    {
+        $units = array('B', 'KB', 'MB', 'GB', 'TB');
+
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+
+        // Uncomment one of the following alternatives
+//         $bytes /= pow(1024, $pow);
+         $bytes /= (1 << (10 * $pow));
+
+        return round($bytes, $precision) . ' ' . $units[$pow];
     }
 }
